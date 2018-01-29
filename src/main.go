a0d63ecede367bb04c3251fdb1af463c8621a8e5
PumpkinSeed <qwer.kocka@gmail.com>
2018-01-29 19:41:54 +0100

    Add vendor and compose file

--- src/main.go
+++ src/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"flag"
+	"math/rand"
+	"time"
+
 	"github.com/PumpkinSeed/durable-nsq-example/src/consumer"
 	"github.com/PumpkinSeed/durable-nsq-example/src/producer"
 )
 
+var consume bool
+var amount int
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	flag.BoolVar(&consume, "consume", false, "Consume the queue")
+	flag.IntVar(&amount, "amount", 10, "Amount of the test messages")
+	flag.Parse()
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randStringRunes(n int) []byte {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	t := time.Now()
+	return []byte(t.Format("2006-01-02 15:04:05") + " - " + string(b))
+}
+
 func main() {
-	go func() {
-		for i := 0; i < 10; i++ {
-			producer.Write([]byte("test"))
+	if consume {
+		go func() {
+			for i := 0; i < amount; i++ {
+				producer.Write(randStringRunes(10))
+			}
+		}()
+		consumer.Start()
+	} else {
+		for i := 0; i < amount; i++ {
+			producer.Write(randStringRunes(10))
 		}
-	}()
-	consumer.Start()
+	}
 }
