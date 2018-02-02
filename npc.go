101b3bc91aec1fb4cfb25f219aceb98f963c35eb
PumpkinSeed <qwer.kocka@gmail.com>
2018-02-02 14:18:42 +0100

    Add new main handler

--- npc.go
+++ npc.go
@@ -1,17 +1,23 @@
 package npc
 
+// T the type of the npc
 type T int
 
 const (
+	// Server type
 	Server T = iota
+
+	// Client type
 	Client
 )
 
+// Main handler
 type Main struct {
 	Server bool
 	Client bool
 }
 
+// New creates a new instance of the Main handler based on the type
 func New(t T) Main {
 	m := Main{}
 	switch t {
